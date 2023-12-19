2fa5c1d2ac7700a57ee1702bac4eb9baba04cdd9
Lucas Seewald <ladams@unisinos.br>
2023-12-19 16:27:53 -0300

    Test commit for new repo.

---
+++ Source/MyProject/Private/GSoundManager.cpp
@@ -0,0 +1,97 @@
+#include "GSoundManager.h"
+#include "Misc/FileHelper.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+// Sets default values
+AGSoundManager::AGSoundManager()
+{
+	FString FullPath = FPaths::ProjectContentDir() / "NextTestNumber.txt";
+
+	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*FullPath))
+	{
+		IFileHandle* FileHandle = FPlatformFileManager::Get().GetPlatformFile().OpenWrite(*FullPath);
+		if (FileHandle)
+		{
+			// Write the number zero to the file
+			FString ZeroString = FString::Printf(TEXT("0"));
+			FileHandle->Write((const uint8*)ZeroString.GetCharArray().GetData(), ZeroString.Len());
+			delete FileHandle;
+		}
+	}
+}
+
+void AGSoundManager::SetCsvFileName(FString fileNameChosen)
+{
+	fileName = fileNameChosen;
+}
+
+void AGSoundManager::AddToCsv(int gabarito, int resposta)
+{
+	tPosition.Add(gabarito);
+	aPosition.Add(resposta);
+
+	if (tPosition.Num() >= 1)
+	{
+		WriteToCsv();
+		tPosition.Empty();
+		aPosition.Empty();
+	}
+}
+
+void AGSoundManager::WriteToCsv()
+{
+	FString FullPath = FPaths::ProjectContentDir() / "NextTestNumber.txt";
+	IFileHandle* FileHandle = FPlatformFileManager::Get().GetPlatformFile().OpenRead(*FullPath, false);
+	if (FileHandle)
+	{
+		FString FileContents;
+		FFileHelper::LoadFileToString(FileContents, *FullPath);
+		fileName = "Teste" + FileContents;
+
+		int32 OutValue = FCString::Atoi(*FileContents);
+
+		// Increment the value
+		OutValue++;
+
+		delete FileHandle;
+
+		FileHandle = FPlatformFileManager::Get().GetPlatformFile().OpenWrite(*FullPath);
+
+		// Save the updated value back to the file
+		FString NumberString = FString::Printf(TEXT("%d"), OutValue);
+		FileHandle->Write((const uint8*)NumberString.GetCharArray().GetData(), NumberString.Len());
+
+		delete FileHandle;
+	}
+
+
+	fileName += ".csv";
+	std::ofstream file;
+	file.open(TCHAR_TO_UTF8(*(FPaths::ProjectContentDir() + fileName)));
+
+	if (!file.is_open())
+	{
+		//std::cout << "Nao deu" << "\n";
+		return;
+	}
+	else
+	{
+		file << "Gabarito ; Respostas" << "\n";
+		for (int i = 0; i < tPosition.Num(); i++)
+		{
+			file << tPosition[i] << ";" << aPosition[i] << ";";
+
+			if (tPosition[i] == aPosition[i]) file << "Acertou";
+			else file << "Errou";
+
+			file << "\n";
+		}
+	}
+
+	file.close();
+}
---
+++ Source/MyProject/Public/GSoundManager.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GSoundManager.generated.h"
+
+UCLASS()
+class MYPROJECT_API AGSoundManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGSoundManager();
+
+	UFUNCTION(BlueprintCallable, Category = "CSVWriter")
+	void SetCsvFileName(FString fileNameChosen);
+
+	UFUNCTION(BlueprintCallable, Category = "CSVWriter")
+	void AddToCsv(int gabarito, int resposta);
+
+	//UFUNCTION(BlueprintCallable, Category = "CSVWriter")
+	void WriteToCsv();
+
+private:
+	TArray<int> tPosition{};
+	TArray<int> aPosition{};
+	FString fileName;
+};
